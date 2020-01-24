25b7cf8c84e89c6d006ea339dc99d57a83b73651
Olivier Clero <olivier.clero@dashlane.com>
2020-01-24 12:28:02 +0100

    Add example project with simple script

---
+++ src/Bijou.Example/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Bijou.Example
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
